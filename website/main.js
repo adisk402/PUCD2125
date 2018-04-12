6cc81b627756e04d86f121b607bbcd7cd059184e
Kamal <adisk402@newschool.edu>
2018-04-11 22:08:32 -0400

    Hover effect on all links
    Aplied hver effect on al .a

--- website/main.js
+++ website/main.js
@@ -1,9 +1,13 @@
 
 
-$( ".content-img" ).on( "click", function() {
+$( ".content-img img" ).on( "click", function(event) {
   $(".overlay").show(); 
+  var imageUrl = $(event.currentTarget).attr('src');
+
+  $('.image-full').css('background-image', 'url(' + imageUrl + ')')
 });
 
 $( "h3" ).on( "click", function() {
   $(".overlay").hide(); 
 });
+
